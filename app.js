69e18288cf10a4ddff96e1cf4ff7c6a915fec8b9
Tanshi Nayak <tanshitn@gmail.com>
2020-05-19 13:06:11 +0530

    add colour

--- app.js
+++ app.js
@@ -5,14 +5,57 @@ let black=document.getElementById('black');
 let eraser=document.getElementById('eraser');
 let clear=document.getElementById('clear');
 function generategrid(){
-let num=prompt("Enter Size",'');
+let num=prompt("Enter Size",'16');
 for (let rows = 0; rows < num; rows++) {
     for(let columns=0;columns<num;columns++){
         let cell = document.createElement('div');
         cell.classList.add('cell');
-        cell.innerHTML=rows+columns;
         container.appendChild(cell);
     }
 };
+container.style.gridTemplateColumns= `repeat(${num},1fr)`;
+container.style.gridTemplateRows= `repeat(${num},1fr)`;
+};
+function erase()
+{
+    let cells = [...document.querySelectorAll(".cell")];
+  cells.forEach((cell) => {
+    cell.addEventListener("mouseover", () => {
+      cell.style.backgroundColor = 'white';
+      cell.style.transition = "0.3s";
+    });
+  });
+};
+function eraserall()
+{
+    let cells = [...document.querySelectorAll(".cell")];
+    cells.forEach((cell) => {
+        cell.style.backgroundColor = 'white';
+        cell.style.transition = "0.3s";
+      });
+};
+function colourrandom(){
+    let cells = [...document.querySelectorAll(".cell")];
+  cells.forEach((cell) => {
+    cell.addEventListener("mouseover", () => {
+        let randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+      cell.style.backgroundColor = randomColor;
+      cell.style.transition = "0.3s";
+    });
+  });
+};
+function colourblack()
+{
+    let cells = [...document.querySelectorAll(".cell")];
+  cells.forEach((cell) => {
+    cell.addEventListener("mouseover", () => {
+      cell.style.backgroundColor = 'black';
+      cell.style.transition = "0.3s";
+    });
+  });
 };
 generate.addEventListener("click",generategrid);
+rainbow.addEventListener("click",colourrandom);
+black.addEventListener("click",colourblack);
+eraser.addEventListener("click",erase);
+clear.addEventListener("click",eraserall);
